088ec1fe4c9f8f41cc66015a7b4e4454e4199936
Replit user <>
2023-12-14 21:47:18 +0000

    presentationcommit

---
+++ script.js
@@ -0,0 +1,25 @@
+let clickHome = document.querySelector("#homeButton");
+
+clickHome.addEventListener('click',function(){
+  window.location.href = "index.html";
+
+});
+
+
+  let clickAboutMe = document.querySelector("#aboutButton");
+
+clickAboutMe.addEventListener('click', function(){
+   window.location.href = "about.html";
+});
+
+let clickProjects = document.querySelector("#projectsButtton");
+
+clickProjects.addEventListener('click', function(){
+  window.location.href = "projects.html";
+});
+
+let clickContact = document.querySelector("#contactButton");
+
+clickContact.addEventListener('click', function(){
+  window.location.href = "contact.html";
+});
